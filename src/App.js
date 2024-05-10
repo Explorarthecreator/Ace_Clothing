2aa92a6b167a595009d82cee9c8753843334918d
Explorarthecreator <fredadejiemma99@gmail.com>
2024-05-10 11:08:47 +0100

    Setting up the routes and the skeleton of the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className=" text-2xl">
-      <h1>
-        App Cleanup
-      </h1>
-    </div>
+    <>
+      <Router>
+        <div className="">
+          <h1>
+            This is the Nav Bar
+          </h1>
+        </div>
+        <main>
+          <Routes>
+            <Route path="/" element={<Homepage/>}/>
+          </Routes>
+        </main>
+      </Router>
+    </>
   );
 }
 
