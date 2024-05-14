deb576cec32729bd7102231730de5b553fef21fa
Explorarthecreator <fredadejiemma99@gmail.com>
2024-05-14 14:30:08 +0100

    Created the add item function for adding items to the cart. Local storage is used here and will only be cleared when a user makes an order.

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,18 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
-
+const cart = JSON.parse(localStorage.getItem('cart'))
+// localStorage.removeItem('cart')
 const initialState = {
-    cart:[]
+    cart:cart? cart :[],
+    // message:null
 }
+// const dispatch = useDispatch()
 
 export const CartSlice = createSlice({
     name:'cart',
     initialState,
     reducers:{
-        reset:(state)=>initialState
-    }
+        reset:(state)=>initialState,
+        addItem:(state,action)=>{
+            // console.log(action.payload);
+            const newCart = action.payload
+
+            if(newCart.size === ''){
+                // state.message = 'Please select a size'
+                // return false
+                toast.error('Please Select a size')
+            }else{
+                state.message = null
+                const  exist = state.cart.find((item)=>item.name === newCart.name )
+
+                if (exist){
+                    toast.error('This item already exists in your cart')
+                }
+                else{
+                    // const oldItems = [...cart,newCart]
+                    let oldItems
+                    if(state.cart.length <=0){
+                        oldItems = [newCart]
+                        console.log("empty");
+                    }
+                    else{
+                        oldItems = [...state.cart,newCart]
+                        console.log("not empty");
+                    }
+                    localStorage.setItem('cart', JSON.stringify(oldItems))
+                    state.cart.push(newCart)
+                    console.log(oldItems);
+                    toast.success('Cart added')
+                }
+                // localStorage.setItem('cart', JSON.stringify(action.payload))
+                // return true
+            }
+        }
+    },
 })
 
-export const {reset} = CartSlice.actions
+export const {reset, addItem} = CartSlice.actions
 
 export default CartSlice.reducer
