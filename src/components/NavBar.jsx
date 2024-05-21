feb4a3ee453783d261fee63dd75108a633e2c015
Explorarthecreator <fredadejiemma99@gmail.com>
2024-05-21 12:46:16 +0100

    Brought it varibales from the auth state to be able show several things depending if the user is logged in or not

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,36 @@
 import { FaCartShopping } from "react-icons/fa6"
 import { useDispatch, useSelector } from "react-redux"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 // import FinishOrder from "./FinishOrder"
 import { show } from "../features/cart/cartSlice"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { useEffect, useState } from "react"
 
 
 function NavBar() {
     const dispatch = useDispatch()
-    const loggedIn = true
+    const location = useLocation()
+    const auth = getAuth()
+    const [name,setName] = useState('')
+    const {loggedIn} = useSelector((state)=>state.auth)
+    const  pathMatchRoute =(route)=>{
+        if(route=== location.pathname){
+            console.log("object");
+            return true
+        }
+    }
+    useEffect(()=>{
+        onAuthStateChanged(auth,(user)=>{
+            if(user){
+                setName(user.displayName)
+            }else{
+                setName('Anon')
+            }
+        })
+    },[auth])
     const {cart} = useSelector((state)=>state.cart)
   return (
     <nav className='w-full overflow-hidden bg-white shadow-xl text-black p-2 fixed top-0 left-0'>
-        {/* <FinishOrder/> */}
         <div className="navbar md:w-11/12 md:m-auto p-1">
             <div className="flex-1">
                 <Link className="text-xl font-encode font-semibold">
@@ -29,7 +48,9 @@ function NavBar() {
                                 </div>
                             </div>
                             <p className=' hidden md:block'>
-                                Hi Emmanuel
+                                Hi {
+                                    name? name : ''
+                                }
                             </p>
                         </div>
                         <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-40">
@@ -41,11 +62,11 @@ function NavBar() {
                             <li><Link>Logout</Link></li>
                         </ul>
                     </div>:
-                    <button className='btn bg-black text-white btn-sm'>
+                    <Link className={`btn flex items-center bg-black text-white btn-sm ${pathMatchRoute('/login')?'hidden':'block'}`} to={'/login'}>
                         Login
-                    </button>
+                    </Link>
                 }
-                <div className="">
+                <div className={`${pathMatchRoute('/login')?'hidden':'block'}`}>
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle" onClick={()=>dispatch(show())}>
                         <div className="indicator">
                             <FaCartShopping size={30}/>
